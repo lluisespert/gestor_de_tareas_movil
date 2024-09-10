1b903947021d81939fc32fe9e8c4beace2a61826
lluisespert <lluisespertc@gmail.com>
2024-09-10 09:02:00 +0200

    Primer paso
    Creando la base de datos interna (Poco a poco, que aún he de analizar lo que realmente me puede hacer falta)

---
+++ Gestor_de_Tareas/Gestor_de_TareasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestor_de_TareasApp.swift
+//  Gestor_de_Tareas
+//
+//  Created by Lluís Espert Cuquerella on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Gestor_de_TareasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gestor_de_Tareas/Model/bbdd.swift
@@ -0,0 +1,33 @@
+//
+//  bbdd.swift
+//  Gestor_de_Tareas
+//
+//  Created by Lluís Espert Cuquerella on 10/9/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+
+class Tareas {
+    
+    @Attribute(.unique) var id: UUID
+    
+    var nombre: String
+    
+    var tipo_tarea: String
+    
+    var contenido_de_tarea: String
+    
+    var prioridad_tarea: String
+    
+    init(id: UUID, nombre: String, tipo_tarea: String, contenido_de_tarea: String, prioridad_tarea: String) {
+        self.id = id
+        self.nombre = nombre
+        self.tipo_tarea = tipo_tarea
+        self.contenido_de_tarea = contenido_de_tarea
+        self.prioridad_tarea = prioridad_tarea
+    }
+    
+}
